57f6ea500e8f11096f8ab9791a0967e7094532d9
Maupeu Mateo <mateo.maupeu@student-cs.fr>
2026-03-31 15:17:27 +0200

    je fais ca

--- code/commande.py
+++ code/commande.py
@@ -1,24 +1,24 @@
 import numpy as np
 
 
-
-def commande_barre_P(cap_cons, cap_comp):
+def commande_barre_P(cap_cons, cap_compas_l):
     """renvoie l'angle de barre en fonction de la différence de cap
 
     Args:
         cap_cons (_type_): _description_
-        cap_comp (_type_): _description_
+        cap_compas_l (array): _description_
 
     Returns:
         float: angle de barre en degré
     """
+    
     Kp=1
     Ki=1
     Kd=1
     theta_barre_max=30   #en degré
-    
-    theta_barre=K*(cap_cons-cap_comp) #correction proportionnel
-    theta_barre=np.sign(theta_barre)*min(theta_barre_max,theta_barre)
+    dcap=cap_cons-cap_compas_l[-1] #différence de cap
+    theta_barre=Kp*(dcap) #correction proportionnel
+    theta_barre=np.sign(theta_barre)*min(theta_barre_max,theta_barre) #on limite l'angle de barre à theta_barre_max
     
     return theta_barre
 
